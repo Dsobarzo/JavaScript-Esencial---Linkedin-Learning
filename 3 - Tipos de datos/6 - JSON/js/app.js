1113c6a64e89a89aeced21e9df7bd121898b9c21
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-14 13:49:56 -0300

    Se añade comentarios
    Se establece el uso de JSON

--- 3 - Tipos de datos/6 - JSON/js/app.js
+++ 3 - Tipos de datos/6 - JSON/js/app.js
@@ -1,6 +1,7 @@
 "use strict"
 
 // JSON => JavaScript Object Notation
+// formato de cambio de datos
 
 var persona = {nombre: 'Sergio', twitter: 'yacafx'};
 
@@ -8,9 +9,12 @@ var personas = [
     {nombre: 'Hugo', twitter: 'dcHugo'},
     {nombre: 'Paco', twitter: 'dcPaco'},
     {nombre: 'Luis', twitter: 'dcLuis'},
+    //se añade var persona al objeto personas
     persona
 ]
 
+//convertir datos con el objeto JSON (cadena de texto)
 var personaJSON = JSON.stringify(persona);
 
+//recuerar de JSON con el metodo parse a un objeto
 var nuevaPersona = JSON.parse(personaJSON)
