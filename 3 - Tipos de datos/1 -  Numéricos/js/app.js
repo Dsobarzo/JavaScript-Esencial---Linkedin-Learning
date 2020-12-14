064174955c0000ee3c8422767655b2998af7010e
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-14 13:18:07 -0300

    Cambio de tipo de var
    Se cambo el tipo de datos - String a int

--- 3 - Tipos de datos/1 -  Numéricos/js/app.js
+++ 3 - Tipos de datos/1 -  Numéricos/js/app.js
@@ -1,11 +1,16 @@
 "use strict"
 
+//dato entero
 var edad = 35;
 
+//dato string (cadena de texto)
 var cantidad = "100";
 
+//para convertir dato string a dato numero "Numbers"
 var nuevaCantidad = Number(cantidad);
 
+// para convertir tus datos a enteros
 parseInt()
 
+//para convertir tus datos a decimal
 parseFloat()
