5236c02b412a81200b88cad0605239a0210db07d
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-14 13:01:11 -0300

    Se añade modo estricto
    Se añade modo estricto para buenas parcticas, se comente algunas lineas de codigo

--- 1 - Conociendo JavaScript/05 - Strict mode/js/app.js
+++ 1 - Conociendo JavaScript/05 - Strict mode/js/app.js
@@ -1,13 +1,13 @@
 "use strict"
-
+//el modo estricto para el uso de buenas practicas
 
 // alert('Hola desde un archivo externo');
 
 // Este es un mensaje de la consola
 console.log('Hola consola');
-
+//se declara la varieble x
 var x; 
-
+//se le asgina valor
 x = "3.1416";
 
 var publicData = "Hola";
