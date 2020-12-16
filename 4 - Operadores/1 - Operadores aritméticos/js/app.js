cd799bb0b1b412ae875187362a10e3dd510e47c3
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-16 14:21:05 -0300

    Se agrego conmentario

--- 4 - Operadores/1 - Operadores aritméticos/js/app.js
+++ 4 - Operadores/1 - Operadores aritméticos/js/app.js
@@ -22,6 +22,7 @@ var division = datoA / datoB;
 console.log('La división de '+ datoA +' / '+ datoB +' es igual a: ', division);
 
 // MODULO O RESIDUO %
+//lo que queda de la divicion.
 var modulo = datoA % datoB;
 console.log('El módulo o residuo de '+ datoA +' % '+ datoB +' es igual a: ', modulo);
 
