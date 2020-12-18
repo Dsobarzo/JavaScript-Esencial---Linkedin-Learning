8a496c5f7dcb777b5719ccd5b6b3a5ddb842fcbd
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-18 14:25:39 -0300

    Se añade comentario sobre las funciones.

--- 7 - Funciones o métodos/1 - Estructura básica de una función/js/app.js
+++ 7 - Funciones o métodos/1 - Estructura básica de una función/js/app.js
@@ -2,7 +2,7 @@
 
 //********************************
 //*** Estructura básica de una función
-
+// se puede llamar FUNCION o METODO
 function saludar() {
     var saludo = "Hola Mundo";
     //console.log(saludo);
