c25785b85fb8c93564db6ae8bc63ee32898351c4
Dsobarzo <41088464+Dsobarzo@users.noreply.github.com>
2020-12-18 16:10:44 -0300

    Se agrego comentario

--- 10 - Trabajando con números/1 - Propiedades númericas/js/app.js
+++ 10 - Trabajando con números/1 - Propiedades númericas/js/app.js
@@ -2,13 +2,13 @@
 
 //********************************
 //*** Propiedades númericas
-
+//MAX VALUE y MIN VALUE es el valor maximo y mino que sooporte JS
 console.log("MAX_VALUE: ", Number.MAX_VALUE);
 
 console.log("MIN_VALUE: ", Number.MIN_VALUE);
-
+//Es el ogjeto infinity, puede ser infinito
 console.log("NEGATIVE_INFINITY: ", Number.NEGATIVE_INFINITY);
 
 console.log("NEGATIVE_INFINITY: ", Number.POSITIVE_INFINITY);
-
+//Not a number. para comparacion si el digito es o no un numero
 console.log("NaN: ", Number.NaN);
